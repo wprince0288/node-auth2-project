d6a1a9c3f60aa26208652075e36371ff944ff004
Walter Prince <wprince0288@gmail.com>
2025-07-05 18:06:08 -0500

    wiip

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -18,6 +18,10 @@ function find() {
       }
     ]
    */
+
+  return db('users')
+    .join('roles', 'users.role_id', 'roles.role_id')
+    .select('user_id', 'username', 'role_name')
 }
 
 function findBy(filter) {
@@ -33,7 +37,20 @@ function findBy(filter) {
         "role_name": "admin",
       }
     ]
+      select
+    user_id,
+    username,
+    password,
+    role_name
+from users
+join roles on
+    users.role_id = roles.role_id
+where users.user_id = 1;
    */
+  return db('users')
+    .join('roles', 'users.role_id', 'roles.role_id')
+    .select('user_id', 'username', 'passwor', 'role_name')
+    .where(filter)
 }
 
 function findById(user_id) {
@@ -47,6 +64,10 @@ function findById(user_id) {
       "role_name": "instructor"
     }
    */
+  return db('users')
+    .join('roles', 'users.role_id', 'roles.role_id')
+    .select('user_id', 'username', 'passwor', 'role_name')
+    .where('users.user_id', user_id).first()
 }
 
 /**
